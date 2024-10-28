7b96bc171ff5785f11c3e5ad96742e03137b593c
Durgesh Konga <kongadurgesh20@gmail.com>
2024-10-28 12:06:24 +0530

    first commit

---
+++ src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        System.out.println(args[0]);
+        System.out.println(args.length);
+    }
+}
